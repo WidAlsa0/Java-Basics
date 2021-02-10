19b5fa02d133f398df1511676e7bb4f480b6df6f
Wid Alsadoon <wid@live.unc.edu>
2021-02-10 11:18:34 -0500

    Submission 3: Jedi Test update #2

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -5,55 +5,85 @@ import java.util.Scanner;
 public class A1Jedi {
 
 	public static void main(String[] args) {
-		
+
 		Scanner scan = new Scanner(System.in);
 
-		// The first part of the code will input data for the items in the store
-		int storeItems = scan.nextInt();
-		String[] nameItems = new String[storeItems];
-		double[] priceItems = new double[storeItems];
-		for (int i = 0; i < storeItems; i++) {
-			nameItems[i] = scan.next();
-			priceItems[i] = scan.nextDouble();
+		// The number of items in the store
+		int total = scan.nextInt();
+
+		// Store name, price, and frequency in arrays
+		String[] itemNameArray = new String[total];
+		double[] itemPriceArray = new double[total];
+		int[] itemAmountArray = new int[total];
+
+		// Given a name and price for each item in the store
+		for (int i = 0; i < total; i++) {
+			String item_name = scan.next();
+			Double item_price = scan.nextDouble();
+			itemNameArray[i] = item_name;
+			itemPriceArray[i] = item_price;
 		}
 
-		int[] itemQuantity = new int[storeItems];
-		int[] customerQuantity = new int[storeItems];
-
-		// The second part of the code will take in the customer information
+		// The number of customers given
 		int customers = scan.nextInt();
 
-		for (int i =0; i<customers; i++) {
-			String First = scan.next();
-			String Last = scan.next();
 
-			int items = scan.nextInt();
+		// Create string array to store customer's first and last name
+		String[] first_array = new String[customers];
+		String[] last_array = new String[customers];
 
-			for (int a =0; a < items; a++) {
+
+		// Create integer array to store how many customers got an item
+		int[] customerItemPurchase = new int[total];
+
+
+
+		// Go through each customer
+		for (int i = 0; i < customers; i++) {
+			String first = scan.next();
+			first_array[i] = first;
+			String last = scan.next();
+			last_array[i] = last;
+
+			int numberOfItems = scan.nextInt();
+
+			// Create an integer array to check if the customer bought an item before
+			int[] customerBought = new int[total];
+
+			for (int a = 0; a < numberOfItems; a++) {
 				int quantity = scan.nextInt();
-				String givenItem = scan.next();
-				int foundItem = -1;
+				String givenItemName = scan.next();
+				int name_element = findItemNameElement(givenItemName, itemNameArray);
+				itemAmountArray[name_element] = itemAmountArray[name_element] + quantity;
+				customerBought[name_element] += 1;
 
-				for (int b = 0; b < storeItems; b++) {
-					if (givenItem.equals(nameItems[b])) {
-						foundItem = b;
-						customerQuantity[foundItem] = customerQuantity[foundItem] + 1;
-					}
-				}
-
-				itemQuantity[foundItem] = itemQuantity[foundItem] + quantity;
+				if (customerBought[name_element] == 1)
+					customerItemPurchase[name_element] += 1;
 			}
 
 		}
 
-		for (int i=0; i < storeItems; i++) {
-			if (itemQuantity[i] == 0) {
-				System.out.println("No customers bought " + nameItems[i]);
+		// Print the statements
+		for (int i = 0; i < total; i++) {
+			if (itemAmountArray[i] == 0) {
+				System.out.println("No customers bought " + itemNameArray[i]);
 			} else {
-				System.out.println(customerQuantity[i] + " customers bought " + itemQuantity[i] + " " + nameItems[i]);
+				System.out.println(customerItemPurchase[i] + " customers bought " + itemAmountArray[i] + " " + itemNameArray[i]);
 			}
+
 		}
 
-		
+
 	}
-}
+
+	private static int findItemNameElement(String givenItemName, String[] itemNameArray) {
+		int element = -1;
+		for (int i = 0; i < itemNameArray.length; i++) {
+			if (givenItemName.equals(itemNameArray[i])) {
+				element = i;
+			}
+		}
+		return element;
+	}
+
+	}
